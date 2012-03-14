7507468530530f66ff8bc118c21ee9dae71c06fd
Michael S. Klishin <michael@novemberain.com>
2012-03-14 11:45:36 +0400

    Use Leiningen 2 aliases

--- project.clj
+++ project.clj
@@ -6,10 +6,7 @@
                  [com.novemberain/langohr "1.0.0-beta1"]
                  [org.clojure/data.json "0.1.2"]]
   :source-paths ["src/clojure"]
-  :profiles {:all {:dependencies [[clojurewerkz/quartzite "1.0.0-beta2"]
-                                  [com.novemberain/langohr "1.0.0-beta1"]
-                                  [org.clojure/data.json "0.1.2"]] }
-             :dev {:resource-paths ["test/resources"],
+  :profiles {:dev {:resource-paths ["test/resources"],
                    :dependencies   [[clj-time "0.3.3"                  :exclusions [org.clojure/clojure]]
                                     [org.clojure/tools.logging "0.2.3" :exclusions [org.clojure/clojure]]
                                     [org.slf4j/slf4j-simple "1.6.2"]
@@ -19,15 +16,8 @@
                                                                  com.sun.jdmk/jmxtools
                                                                  com.sun.jmx/jmxri]]]}
              :1.4 {:resource-paths ["test/resources"],
-                   :dependencies   [[org.clojure/clojure "1.4.0-beta4"]
-                                    [clj-time "0.3.3"                  :exclusions [org.clojure/clojure]]
-                                    [org.clojure/tools.logging "0.2.3" :exclusions [org.clojure/clojure]]
-                                    [org.slf4j/slf4j-simple "1.6.2"]
-                                    [org.slf4j/slf4j-api "1.6.2"]
-                                    [log4j "1.2.16" :exclusions [javax.mail/mail
-                                                                 javax.jms/jms
-                                                                 com.sun.jdmk/jmxtools
-                                                                 com.sun.jmx/jmxri]]]}}
+                   :dependencies   [[org.clojure/clojure "1.4.0-beta4"]]}}
+  :aliases  { "all" ["with-profile" "dev:dev,1.4"] }
   :aot [clojurewerkz.quartzite.listeners.amqp.PublishingSchedulerListener]
   :java-source-paths ["src/java"]
   :warn-on-reflection true)
