3c40cbe9eec9bd209e3a9fe093179c2bed2d476d
Michael Klishin <michael@clojurewerkz.org>
2014-01-19 09:53:01 +0400

    Update Clojure profiles

--- project.clj
+++ project.clj
@@ -15,10 +15,9 @@
                                                                  javax.jms/jms
                                                                  com.sun.jdmk/jmxtools
                                                                  com.sun.jmx/jmxri]]]}
-             :1.3 {:resource-paths ["test/resources"],
-                   :dependencies   [[org.clojure/clojure "1.3.0"]]}
-             :1.5 {:dependencies [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]]}}
-  :aliases  { "all" ["with-profile" "dev:dev,1.3:dev,1.5"] }
+             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}}
+  :aliases  { "all" ["with-profile" "dev:dev,1.4:dev,1.6"] }
   :repositories {"sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
                              :snapshots false
                              :releases {:checksum :fail :update :always}}
