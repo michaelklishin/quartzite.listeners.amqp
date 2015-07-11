e8867b87a1f8b1f1b52d75325f0cb8f10cc5a1fc
Michael Klishin <michael@clojurewerkz.org>
2015-07-12 01:17:05 +0300

    Clojure master is now 1.8.0-master-SNAPSHOT

--- project.clj
+++ project.clj
@@ -17,7 +17,7 @@
                                                                  com.sun.jmx/jmxri]]]}
              :1.5    {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :1.7    {:dependencies [[org.clojure/clojure "1.7.0"]]}
-             :master {:dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]]}}
+             :master {:dependencies [[org.clojure/clojure "1.8.0-master-SNAPSHOT"]]}}
   :aliases  { "all" ["with-profile" "dev:dev,1.5:dev,1.7"] }
   :repositories {"sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
                              :snapshots false
